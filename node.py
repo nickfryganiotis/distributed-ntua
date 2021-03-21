49b13941bd82a39b706315083eea605424d04787
nickfryganiotis <nikolasfryganiotis@yahoo.com>
2021-03-21 22:29:54 +0200

    return-values

--- node.py
+++ node.py
@@ -1,14 +1,14 @@
-from finger import Finger 
-class Node:                                                                                                                                                                                          
-  def __init__(self,identifier,ip,port,finger_table = [],predecessor = None,song_list = {}):
+from finger import Finger
+class Node:
+  def __init__(self,identifier,host,port,finger_table = [],predecessor = {},song_list = {}):
      self.identifier = identifier
-     self.ip = ip
+     self.host = host
      self.port = port
      self._finger_table = finger_table
      self._predecessor = predecessor
      self._song_list = song_list
   def get_url(self):
-     return "http://"+self.ip+":"+str(port)
+     return "http://"+self.host+":"+str(port)
 
   def get_finger_table(self):
     return self._finger_table
@@ -16,11 +16,12 @@ class Node:
   def get_predecessor(self):
     return self._predecessor
 
-  def set_predecessor(self,predecessor):
-    self._predecessor = predecessor
+  def set_predecessor(self,predecessor_host,predecessor_port):
+    self._predecessor['host'] = predecessor_host
+    self._predecessor['port'] = predecessor_port
 
-  def set_node_to_finger_table(self,ind,n):
-    self.finger[ind].set_node(n)
+  def set_node_to_finger_table(self,ind,node_host,node_port):
+    self._finger_table[ind].set_node(node_host,node_port)
 
   def add_finger_to_finger_table(self,f):
     self._finger_table.append(f)
@@ -28,5 +29,5 @@ class Node:
   def add_to_song_list(self,key,value):
     self._song_list[key] = value
 
-  def delete_from_song_alist(self,key): 
-      del self._song_list[key]                                                                                                                                                                                                                                                
+  def delete_from_song_alist(self,key):
+    del self._song_list[key]
